15485aafe294892051db71e944bc9147e8b76a57
suresh407 <suresh.ch407@gmail.com>
2017-01-10 09:18:37 +0000

    third

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110045403) do
+ActiveRecord::Schema.define(version: 20170110075110) do
 
   create_table "contact_pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "first_name"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20170110045403) do
     t.text     "other",           limit: 65535
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
-    t.float    "price",           limit: 24
+    t.text     "price",           limit: 65535
   end
 
   create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
